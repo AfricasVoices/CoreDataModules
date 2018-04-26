4dc85a9f2ed34c2a6112938c437b2b7d852266a6
Alexander Simpson <as2388@cam.ac.uk>
2018-04-26 10:52:31 +0100

    Add (passing) unit tests for DemographicCleaner.clean_gender

---
+++ test/test_demographic_cleaner.py
@@ -0,0 +1,18 @@
+import unittest
+
+from cleaners.demographic_cleaner import DemographicCleaner
+
+
+class TestDemographicCleaner(unittest.TestCase):
+    def test_clean_gender(self):
+        self.assertEqual(DemographicCleaner.clean_gender("m"), "M")
+        self.assertEqual(DemographicCleaner.clean_gender("F"), "F")
+        self.assertEqual(DemographicCleaner.clean_gender("Male"), "M")
+        self.assertEqual(DemographicCleaner.clean_gender("female"), "F")
+        self.assertEqual(DemographicCleaner.clean_gender("man"), "M")
+        self.assertEqual(DemographicCleaner.clean_gender("woMAn"), "F")
+
+        self.assertEqual(DemographicCleaner.clean_gender(""), "NC")
+        self.assertEqual(DemographicCleaner.clean_gender("f."), "NC")
+        self.assertEqual(DemographicCleaner.clean_gender(" f "), "NC")
+        self.assertEqual(DemographicCleaner.clean_gender("men"), "NC")
