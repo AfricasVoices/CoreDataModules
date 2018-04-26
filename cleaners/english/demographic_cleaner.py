fd7ff106f1160d469900211bf9143329365394cd
Alexander Simpson <as2388@cam.ac.uk>
2018-04-26 10:38:12 +0100

    Add simple example DemographicCleaner which cleans English genders

---
+++ cleaners/english/demographic_cleaner.py
@@ -0,0 +1,19 @@
+class DemographicCleaner:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def clean_gender(cls, string):
+        """
+        Converts a string describing gender to M, F, or NC.
+        :param string: Text to be cleaned.
+        :return: "M" if male, "F" if female, "NC" if gender could not automatically be identified.
+        """
+        string = string.lower()
+        if string == "m" or string == "male" or string == "man":
+            return "M"
+        elif string == "f" or string == "female" or string == "woman":
+            return "F"
+        else:
+            return "NC"
