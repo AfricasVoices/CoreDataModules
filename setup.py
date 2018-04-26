469ed338c22297f7296e315d0d185ddc869a4fbc
Alexander Simpson <as2388@cam.ac.uk>
2018-04-26 11:15:25 +0100

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="CoreDataModules",
+    version="0.1",
+    url="https://github.com/AfricasVoices/CoreDataModules",
+    packages=["core_data_modules"]
+)
