335004e4d65888e4903e0bc135feb69a67593a46
Alexander Simpson <as2388@cam.ac.uk>
2018-04-30 11:13:10 +0100

    Revert add Dockerfile/circle.config directly to master

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ setup(
     name="CoreDataModules",
     version="0.1",
     url="https://github.com/AfricasVoices/CoreDataModules",
-    packages=["core_data_modules"],
-    # setup_requires=["pytest-runner"],
-    # tests_require=["pytest"]
+    packages=["core_data_modules"]
 )
--- test_runner.py
+++
@@ -1,4 +0,0 @@
-import xmlrunner
-import unittest
-
-xmlrunner.XMLTestRunner(output="tests_results").run(unittest.TestLoader().discover("./tests"))
