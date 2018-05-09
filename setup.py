9f2b3c823bec02414ff0e7905ec362bde447b426
Alexander Simpson <as2388@cam.ac.uk>
2018-05-09 12:09:49 +0100

    Add dependencies to install_requires.

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
     url="https://github.com/AfricasVoices/CoreDataModules",
     packages=["core_data_modules"],
     setup_requires=["pytest-runner"],
+    install_requires=["deprecation==2.0.2"],
     tests_require=["pytest", "deprecation==2.0.2"]
 )
