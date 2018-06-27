8df8f34cd41666c717876a54b173c81efb739167
Alexander Simpson <as2388@cam.ac.uk>
2018-06-27 18:08:35 +0100

    Update setup.py version to match version about to be released.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="CoreDataModules",
-    version="0.2.3",
+    version="0.2.2",
     url="https://github.com/AfricasVoices/CoreDataModules",
     packages=["core_data_modules"],
     setup_requires=["pytest-runner"],
