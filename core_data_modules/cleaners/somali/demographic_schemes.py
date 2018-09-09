016e336e10fe42b7a2df8b8c22323fb286239b84
Alexander Simpson <as2388@cam.ac.uk>
2018-09-09 12:30:39 +0300

    Re-implement DemographicScheme members as functions

--- core_data_modules/cleaners/somali/demographic_schemes.py
+++ core_data_modules/cleaners/somali/demographic_schemes.py
@@ -1,7 +1,29 @@
+from core_data_modules.cleaners import Codes, somali
+from core_data_modules.cleaners.code_scheme import CodeScheme
+
+
 class DemographicSchemes(object):
-    # YES_NO = CodeScheme.with_missing_codes([Codes.YES, Codes.NO])
-    # GENDER = CodeScheme.with_missing_codes([Codes.MALE, Codes.FEMALE])
-    # URBAN_RURAL = CodeScheme.with_missing_codes([Codes.URBAN, Codes.RURAL])
-    # DISTRICT = CodeScheme.with_missing_codes(somali.DemographicPatterns.somalia_districts.keys())
-    # AGE = CodeScheme.with_missing_codes([str(i) for i in range(1, 101)])
-    pass
+    @staticmethod
+    def yes_no(scheme_id=1, scheme_name="Yes/No", with_missing=True):
+        return CodeScheme(scheme_id=scheme_id, scheme_name=scheme_name, add_codes_for_missing=with_missing,
+                          code_names=[Codes.YES, Codes.NO])
+
+    @staticmethod
+    def gender(scheme_id=1, scheme_name="Gender", with_missing=True):
+        return CodeScheme(scheme_id=scheme_id, scheme_name=scheme_name, add_codes_for_missing=with_missing,
+                          code_names=[Codes.MALE, Codes.FEMALE])
+
+    @staticmethod
+    def urban_rural(scheme_id=1, scheme_name="Urban/Rural", with_missing=True):
+        return CodeScheme(scheme_id=scheme_id, scheme_name=scheme_name, add_codes_for_missing=with_missing,
+                          code_names=[Codes.URBAN, Codes.RURAL])
+
+    @staticmethod
+    def somalia_district(scheme_id=1, scheme_name="Somalia District", with_missing=True):
+        return CodeScheme(scheme_id=scheme_id, scheme_name=scheme_name, add_codes_for_missing=with_missing,
+                          code_names=somali.DemographicPatterns.somalia_districts.keys())
+
+    @staticmethod
+    def age(scheme_id=1, scheme_name="Age", with_missing=True):
+        return CodeScheme(scheme_id=scheme_id, scheme_name=scheme_name, add_codes_for_missing=with_missing,
+                          code_names=[str(i) for i in range(1, 101)])
