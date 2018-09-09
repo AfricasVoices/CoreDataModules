453ead3e55c8d82b663ad4f1b0860aa9edbfc5c2
Alexander Simpson <as2388@cam.ac.uk>
2018-09-09 14:52:45 +0300

    Fix broken CodeScheme export test

--- tests/cleaners/test_code_scheme.py
+++ tests/cleaners/test_code_scheme.py
@@ -15,9 +15,9 @@ class TestCodeScheme(unittest.TestCase):
         shutil.rmtree(self.test_dir)
 
     def test_export_to_coda_scheme_file(self):
-        file_path = path.join(".", "coda_scheme_export_test.csv")
+        file_path = path.join(self.test_dir, "coda_scheme_export_test.csv")
 
-        code_scheme = CodeScheme(scheme_id=2, name="YesNo", code_names=["yes", "no"])
+        code_scheme = CodeScheme(scheme_id=2, name="YesNo", code_names=["yes", "no"], add_codes_for_missing=True)
         with open(file_path, "w") as f:
             code_scheme.export_to_coda_scheme_file(f)
         self.assertTrue(filecmp.cmp(file_path, "tests/cleaners/resources/coda_export_expected_code_scheme.csv"))
