92136c7fe24fd4d5d4f92fda8165330f66077d95
Alexander Simpson <as2388@cam.ac.uk>
2018-09-09 12:16:37 +0300

    Implement CodeScheme.export_to_coda_scheme_file

--- core_data_modules/cleaners/code_scheme.py
+++ core_data_modules/cleaners/code_scheme.py
@@ -1,21 +1,69 @@
+import six
+
 from core_data_modules.cleaners import Codes
+
+if six.PY2:
+    import unicodecsv as csv
+if six.PY3:
+    import csv
+
+
+class Code(object):
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
 
 
 class CodeScheme(object):
-    def __init__(self, codes):
+    def __init__(self, scheme_id=1, scheme_name="default", code_names=None):
         """
         :param codes: Names of codees this scheme should contain
         :type codes: iterable of str
         """
-        self._codes = list(codes)
+        self.scheme_id = scheme_id
+        self.scheme_name = scheme_name
+
+        self.codes = []
+        if code_names is None:
+            code_names = []
+        next_code_id = 1
+        for code_name in code_names:
+            self.codes.append(Code(code_name, "{}-{}".format(scheme_id, next_code_id)))
+            next_code_id += 1
 
     @classmethod
-    def with_missing_codes(cls, codes):
-        codes = list(codes)
-        codes.append("NC")  # TODO: Change to Codes.NOT_CODED
-        codes.append(Codes.STOP)
+    def with_missing_codes(cls, code_names, scheme_id=1, scheme_name="default"):
+        code_names = list(code_names)
+        code_names.append("NC")  # TODO: Change to Codes.NOT_CODED
+        code_names.append(Codes.STOP)
 
-        return cls(codes)
+        return cls(scheme_id=scheme_id, scheme_name=scheme_name, code_names=code_names)
 
     def export_to_coda_scheme_file(self, f):
-        pass
+        """
+        Exports this CodeScheme object to a CSV which can be uploaded to Coda.
+
+        :param f: File to write code scheme to.
+        :type f: file-like
+        """
+        headers = ["scheme_id", "scheme_name", "code_id", "code_value", "code_colour", "code_shortcut",
+                   "code_words", "code_regex", "code_regex_modifier"]
+
+        dialect = csv.excel
+        dialect.delimiter = ";"
+
+        writer = csv.DictWriter(f, fieldnames=headers, dialect=dialect, lineterminator="\n")
+        writer.writeheader()
+
+        for code in self.codes:
+            writer.writerow({
+                "scheme_id": self.scheme_id,
+                "scheme_name": self.scheme_name,
+                "code_id": code.id,
+                "code_value": code.name,
+                "code_colour": "",
+                "code_shortcut": "",
+                "code_words": "",
+                "code_regex": "",
+                "code_regex_modifier": ""
+            })
---
+++ tests/cleaners/test_code_scheme.py
@@ -0,0 +1,23 @@
+import filecmp
+import shutil
+import tempfile
+import unittest
+from os import path
+
+from core_data_modules.cleaners.code_scheme import CodeScheme
+
+
+class TestCodeScheme(unittest.TestCase):
+    def setUp(self):
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        shutil.rmtree(self.test_dir)
+
+    def test_export_to_coda_scheme_file(self):
+        file_path = path.join(self.test_dir, "coda_scheme_export_test.csv")
+
+        code_scheme = CodeScheme.with_missing_codes(["yes", "no"], scheme_id=2, scheme_name="YesNo")
+        with open(file_path, "w") as f:
+            code_scheme.export_to_coda_scheme_file(f)
+        self.assertTrue(filecmp.cmp(file_path, "tests/cleaners/resources/coda_export_expected_code_scheme.csv"))
