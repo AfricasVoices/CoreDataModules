058c0277c9264d3a97bfa845cd2b3f4927d82b02
Alexander Simpson <as2388@cam.ac.uk>
2018-09-11 17:54:00 +0100

    v0.5.4 version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="CoreDataModules",
-    version="0.5.3",
+    version="0.5.4",
     url="https://github.com/AfricasVoices/CoreDataModules",
     packages=["core_data_modules"],
     setup_requires=["pytest-runner"],
