b85b1f57f2721ac1c6547dcc1a64cc1b8d159817
Alexander Simpson <as2388@cam.ac.uk>
2018-09-13 11:44:33 +0100

    Move Somali districts to constants in new SomaliaCodes class.
    Also moves the Somali network operators into SomaliaCodes

---
+++ core_data_modules/cleaners/codes/codes.py
@@ -0,0 +1,17 @@
+class Codes(object):
+    TRUE = "true"
+    FALSE = "false"
+
+    MALE = "male"
+    FEMALE = "female"
+
+    YES = "yes"
+    NO = "no"
+
+    URBAN = "urban"
+    RURAL = "rural"
+
+    TRUE_MISSING = "NA"
+    SKIPPED = "NS"
+    NOT_LOGICAL = "NL"
+    NOT_CODED = None  # TODO: Change to "NC"
