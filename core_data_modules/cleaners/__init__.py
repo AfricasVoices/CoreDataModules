de66eef1c90d66319dd4eb65986dc1d4436fd871
Alexander Simpson <as2388@cam.ac.uk>
2018-11-19 17:21:52 +0000

    Temporarily switch import order

--- core_data_modules/cleaners/__init__.py
+++ core_data_modules/cleaners/__init__.py
@@ -1,6 +1,6 @@
 from .character_cleaner import CharacterCleaner
-from .code_translators import CodeTranslator
 from .codes import Codes
+from .code_translators import CodeTranslator
 from .digit_cleaner import DigitCleaner
 from .phone_cleaner import PhoneCleaner
 from .regex_utils import RegexUtils
