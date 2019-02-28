28aab2d88526bee317a60732b9a067c22c8009a5
Alexander Simpson <as2388@cam.ac.uk>
2019-02-28 14:28:06 +0000

    Add basic logging library

---
+++ core_data_modules/logging/logging.py
@@ -0,0 +1,34 @@
+from core_data_modules.util import TimeUtils
+
+
+class LogLevel(object):
+    def __init__(self, string_value, numeric_value):
+        self.string_value = string_value
+        self.numeric_value = numeric_value
+
+
+class LogLevels(object):
+    ERROR = LogLevel("ERROR", 40)
+    WARNING = LogLevel("WARNING", 30)
+    INFO = LogLevel("INFO", 20)
+    DEBUG = LogLevel("DEBUG", 10)
+
+
+class Logger(object):
+    def __init__(self, name):
+        self.name = name
+
+    def log(self, log_level, message):
+        print(f"{TimeUtils.utc_now_as_iso_string()} - {log_level.string_value} - {self.name}: {message}")
+
+    def error(self, message):
+        self.log(LogLevels.ERROR, message)
+
+    def warning(self, message):
+        self.log(LogLevels.WARNING, message)
+
+    def info(self, message):
+        self.log(LogLevels.INFO, message)
+
+    def debug(self, message):
+        self.log(LogLevels.DEBUG, message)
