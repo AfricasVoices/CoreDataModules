8bf52db4ae3246ce3e84d89466841718a5ed93a4
Alexander Simpson <as2388@cam.ac.uk>
2019-11-21 17:02:28 +0000

    Move the scheme id normalisation code to the correct importer

--- core_data_modules/traced_data/io.py
+++ core_data_modules/traced_data/io.py
@@ -255,10 +255,6 @@ class TracedDataCodaV2IO(object):
                 if labels is not None:
                     # Append each label that was assigned to this message for this scheme to the TracedData.
                     for label in reversed(labels):
-                        # Normalise the label's scheme id
-                        assert label.scheme_id.startswith(scheme.scheme_id)
-                        label.scheme_id = scheme.scheme_id
-
                         td.append_data({key_of_coded: label.to_dict()},
                                        Metadata(user, Metadata.get_call_location(), TimeUtils.utc_now_as_iso_string()))
 
@@ -324,6 +320,10 @@ class TracedDataCodaV2IO(object):
                     # Update the relevant label in this traced data's list of labels with the new label,
                     # and append the whole new list to the traced data.
                     td_labels_lut[label.scheme_id] = label
+
+                    # Normalise the label's scheme id
+                    assert label.scheme_id.startswith(scheme.scheme_id)
+                    label.scheme_id = scheme.scheme_id
 
                     td_labels = list(td_labels_lut.values())
                     td.append_data({coded_key: [label.to_dict() for label in td_labels]},
