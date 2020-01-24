a90bc8e120a5ce244949dd4af1d5130b52ae0ac2
Alexander Simpson <as2388@cam.ac.uk>
2020-01-24 15:14:45 +0000

    Improve the performance of get_call_location

--- core_data_modules/traced_data/traced_data.py
+++ core_data_modules/traced_data/traced_data.py
@@ -46,15 +46,18 @@ class Metadata(object):
         """
         Returns the location where this function was called from.
 
-        :return Caller location in the format 'path/to/file.py:line:function_name'
+        :return Caller location in the format 'path/to/file.py:line_number:function_name'
         :rtype: str
         """
-        frame = inspect.stack()[1]  # Access the previous frame to find out where this function was called from.
-        f = frame[1]
-        line = frame[2]
-        name = frame[3]
+        # Access the previous frame to find out where this function was called from.
+        # Setting context to 0 prevents collection of the surrounding lines in the source file, which substantially
+        # improves performance.
+        frame = inspect.getframeinfo(inspect.currentframe().f_back, context=0)
+        file_path = frame[0]
+        line_number = frame[1]
+        function_name = frame[2]
 
-        return "{}:{}:{}".format(f, str(line), name)
+        return "{}:{}:{}".format(file_path, str(line_number), function_name)
 
     @staticmethod
     def get_function_location(func):
