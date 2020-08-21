9c3926ff9aafed3e87c059867b4e53b86d290208
Alexander Simpson <as2388@cam.ac.uk>
2020-08-21 16:39:53 +0100

    Merge tests for the validators into a single file

--- tests/data_models/test_validators.py
+++ tests/data_models/test_validators.py
@@ -6,15 +6,51 @@ from core_data_modules.data_models import validators
 
 
 class TestValidators(unittest.TestCase):
+    def _expect_validation_failure(self, f, expected_assertion_error_message):
+        try:
+            f()
+        except AssertionError as e:
+            assert str(e) == expected_assertion_error_message
+            return
+
+        self.fail("Invalid data passed validation")
+
+    def test_validate_string(self):
+        validators.validate_string("Test")
+        validators.validate_string("åø")
+
+        self._expect_validation_failure(lambda: validators.validate_string(4), " not a string")
+
+    def test_validate_iso_string(self):
+        validators.validate_iso_string("2018")
+        validators.validate_iso_string("2018-11-10T13:00:00")
+        validators.validate_iso_string("2018-11-10T13:00Z")
+        validators.validate_iso_string("2018-11-10T13:00:05+00:00")
+        validators.validate_iso_string("2018-11-10T13:00:05+03:00")
+
+        self._expect_validation_failure(lambda: validators.validate_iso_string("5/12/2018"), " not an ISO string")
+        self._expect_validation_failure(lambda: validators.validate_iso_string("05/12/2018"), " not an ISO string")
+        self._expect_validation_failure(lambda: validators.validate_iso_string("2018-11-1013:00"), " not an ISO string")
+
+    def test_validate_utc_iso_string(self):
+        validators.validate_utc_iso_string("2018-11-10T13:00:05Z")
+        validators.validate_utc_iso_string("2018-11-10T13:00:05+00:00")
+
+        self._expect_validation_failure(
+            lambda: validators.validate_utc_iso_string("2018-11-10T13:00:00"), " not a UTC ISO string")
+
+        self._expect_validation_failure(
+            lambda: validators.validate_utc_iso_string("2018-11-10T13:00:05+03:00"), " not a UTC ISO string")
+
     def test_validate_datetime(self):
         dt = isoparse("2019-05-10T12:37:04+03:00")
         self.assertEqual(dt, validators.validate_datetime(dt))
 
-        with self.assertRaisesRegex(AssertionError, "dt not a datetime"):
-            validators.validate_datetime("2019-05-10T12:37:04+03:00", "dt")
+        self._expect_validation_failure(
+            lambda: validators.validate_datetime("2019-05-10T12:37:04+03:00", "dt"), "dt not a datetime")
 
-        with self.assertRaisesRegex(AssertionError, "dt not timezone-aware"):
-            validators.validate_datetime(isoparse("2019-05-10T12:37:04"), "dt")
+        self._expect_validation_failure(
+            lambda: validators.validate_datetime(isoparse("2019-05-10T12:37:04"), "dt"), "dt not timezone-aware")
 
     def test_validate_url(self):
         # Test valid URLs
@@ -28,8 +64,8 @@ class TestValidators(unittest.TestCase):
             self.assertEqual(url, validators.validate_url(url))
 
         # Test invalid URLs
-        with self.assertRaisesRegex(AssertionError, "test_url not a valid URL"):
-            validators.validate_url("abc://[", variable_name="test_url")
+        self._expect_validation_failure(
+            lambda: validators.validate_url("abc://[", variable_name="test_url"), "test_url not a valid URL")
 
-        with self.assertRaisesRegex(AssertionError, " not a URL with scheme gs"):
-            validators.validate_url("http://bucket/blob", scheme="gs")
+        self._expect_validation_failure(
+            lambda: validators.validate_url("http://bucket/blob", scheme="gs"), " not a URL with scheme gs")
