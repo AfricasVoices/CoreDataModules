df25e0b43fe05eec2e3717a23f5d4ba3c67e42dc
Alexander Simpson <as2388@cam.ac.uk>
2021-01-14 16:44:10 +0000

    Partially implement engagement_counts in core

---
+++ core_data_modules/analysis/engagement_analysis.py
@@ -0,0 +1,33 @@
+from collections import OrderedDict
+
+from core_data_modules.analysis import analysis_utils
+
+
+def engagement_counts(messages, individuals, consent_withdrawn_field, analysis_configurations):
+    engagement_counts = OrderedDict()  # of dataset name to counts
+
+    for config in analysis_configurations:
+        engagement_counts[config.dataset_name] = OrderedDict({
+            "Dataset": config.dataset_name,
+
+            "Total Messages": "-", # Can't report this for individual weeks because the data has been overwritten with "STOP"
+            "Total Messages with Opt-Ins": len(analysis_utils.filter_opt_ins(messages, consent_withdrawn_field, [config])),
+            # "Total Labelled Messages": len(analysis_utils.filter_fully_labelled(messages, consent_withdrawn_field, [config])),
+            # "Total Relevant Messages": len(analysis_utils.filter_relevant(messages, consent_withdrawn_field, [config])),
+
+            "Total Participants": "-",
+            "Total Participants with Opt-Ins": len(analysis_utils.filter_opt_ins(individuals, consent_withdrawn_field, [config]))
+            # "Total Relevant Participants": len(AnalysisUtils.filter_relevant(individuals, consent_withdrawn_field, [config]))
+        })
+
+    engagement_counts["Total"] = OrderedDict({
+        "Dataset": "Total",
+
+        "Total Messages": len(messages),
+        "Total Messages with Opt-Ins": len(analysis_utils.filter_opt_ins(messages, consent_withdrawn_field, analysis_configurations)),
+
+        "Total Participants": len(individuals),
+        "Total Participants with Opt-Ins": len(analysis_utils.filter_opt_ins(individuals, consent_withdrawn_field, analysis_configurations))
+    })
+
+    return engagement_counts
