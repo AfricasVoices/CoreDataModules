25b370496fef929692355c39d99be7914637c230
Alexander Simpson <as2388@cam.ac.uk>
2021-01-15 15:06:19 +0000

    Tidy up terminology (settled on theme_configurations and breakdown_configurations)

--- core_data_modules/analysis/theme_distributions.py
+++ core_data_modules/analysis/theme_distributions.py
@@ -13,26 +13,26 @@ def _normal_codes(codes):
     return [code for code in codes if code.code_type == CodeTypes.NORMAL]
 
 
-def _make_survey_counts_dict(survey_configurations):
-    survey_counts = OrderedDict()
+def _make_breakdowns_dict(breakdown_configurations):
+    breakdowns = OrderedDict()
 
-    survey_counts["Total Participants"] = 0
-    survey_counts["Total Participants %"] = None
+    breakdowns["Total Participants"] = 0
+    breakdowns["Total Participants %"] = None
 
-    for config in survey_configurations:
+    for config in breakdown_configurations:
         for code in _non_stop_codes(config.code_scheme.codes):
-            survey_counts[f"{config.dataset_name}:{code.string_value}"] = 0
-            survey_counts[f"{config.dataset_name}:{code.string_value} %"] = None
+            breakdowns[f"{config.dataset_name}:{code.string_value}"] = 0
+            breakdowns[f"{config.dataset_name}:{code.string_value} %"] = None
 
-    return survey_counts
+    return breakdowns
 
 
-def _update_survey_counts_dict(survey_counts, individual, survey_configurations):
-    survey_counts["Total Participants"] += 1
+def _update_breakdowns(breakdowns, individual, breakdown_configurations):
+    breakdowns["Total Participants"] += 1
 
-    for config in survey_configurations:
+    for config in breakdown_configurations:
         for code in _non_stop_codes(analysis_utils.get_codes_from_td(individual, config)):
-            survey_counts[f"{config.dataset_name}:{code.string_value}"] += 1
+            breakdowns[f"{config.dataset_name}:{code.string_value}"] += 1
 
 
 def _compute_percentage_str(x, y):
@@ -42,80 +42,79 @@ def _compute_percentage_str(x, y):
         return str(round(x / y * 100, 1))
 
 
-def _set_survey_counts_percentages(survey_counts, total_survey_counts, survey_configurations):
-    survey_counts["Total Participants %"] = \
-        _compute_percentage_str(survey_counts["Total Participants"], total_survey_counts["Total Participants"])
+def _compute_breakdown_percentages(breakdowns, total_breakdowns, breakdown_configurations):
+    breakdowns["Total Participants %"] = \
+        _compute_percentage_str(breakdowns["Total Participants"], total_breakdowns["Total Participants"])
 
-    for config in survey_configurations:
+    for config in breakdown_configurations:
         for code in _non_stop_codes(config.code_scheme.codes):
             theme_name = f"{config.dataset_name}:{code.string_value}"
-            survey_counts[f"{theme_name} %"] = \
-                _compute_percentage_str(survey_counts[theme_name], total_survey_counts[theme_name])
+            breakdowns[f"{theme_name} %"] = \
+                _compute_percentage_str(breakdowns[theme_name], total_breakdowns[theme_name])
 
 
-def _compute_theme_distributions_for_rqa(individuals, consent_withdrawn_field, theme_config, survey_configurations):
-    themes = OrderedDict()  # of rqa code -> (dict of survey_code -> dict of count/%)
+def _compute_theme_distributions_for_dataset(individuals, consent_withdrawn_field, theme_config, breakdown_configurations):
+    themes = OrderedDict()  # of theme_config dataset name -> theme -> (dict of breakdown code -> dict of count/%)
 
-    # For each code in the rqa code scheme, add an entry
-    themes["Total Relevant Participants"] = _make_survey_counts_dict(survey_configurations)
+    themes["Total Relevant Participants"] = _make_breakdowns_dict(breakdown_configurations)
     for code in _non_stop_codes(theme_config.code_scheme.codes):
-        themes[f"{theme_config.dataset_name}_{code.string_value}"] = _make_survey_counts_dict(survey_configurations)
+        themes[f"{theme_config.dataset_name}_{code.string_value}"] = _make_breakdowns_dict(breakdown_configurations)
 
     # Iterate over the individuals, increasing the counts as needed
     for ind in individuals:
         if analysis_utils.relevant(ind, consent_withdrawn_field, theme_config):
-            _update_survey_counts_dict(themes["Total Relevant Participants"], ind, survey_configurations)
+            _update_breakdowns(themes["Total Relevant Participants"], ind, breakdown_configurations)
 
         for code in analysis_utils.get_codes_from_td(ind, theme_config):
-            _update_survey_counts_dict(themes[f"{theme_config.dataset_name}_{code.string_value}"],
-                                       ind, survey_configurations)
+            _update_breakdowns(themes[f"{theme_config.dataset_name}_{code.string_value}"],
+                               ind, breakdown_configurations)
 
-    _set_survey_counts_percentages(
+    _compute_breakdown_percentages(
         themes["Total Relevant Participants"], themes["Total Relevant Participants"],
-        survey_configurations
+        breakdown_configurations
     )
 
     for code in _normal_codes(theme_config.code_scheme.codes):
-        _set_survey_counts_percentages(
+        _compute_breakdown_percentages(
             themes[f"{theme_config.dataset_name}_{code.string_value}"], themes["Total Relevant Participants"],
-            survey_configurations
+            breakdown_configurations
         )
 
     return themes
 
 
-def compute_theme_distributions(individuals, consent_withdrawn_field, theme_configurations, survey_configurations):
+def compute_theme_distributions(individuals, consent_withdrawn_field, theme_configurations, breakdown_configurations):
     theme_distributions = OrderedDict()  # of dataset_name -> theme ->
 
     individuals = analysis_utils.filter_opt_ins(individuals, consent_withdrawn_field, theme_configurations)
 
-    # For each theme configuration, create a dict of theme dataset_name -> (dict of survey code -> count/%)
+    # For each theme configuration, create a dict of theme dataset_name -> (dict of breakdown code -> count/%)
     for theme_config in theme_configurations:
-        theme_distributions[theme_config.dataset_name] = _compute_theme_distributions_for_rqa(
-            individuals, consent_withdrawn_field, theme_config, survey_configurations
+        theme_distributions[theme_config.dataset_name] = _compute_theme_distributions_for_dataset(
+            individuals, consent_withdrawn_field, theme_config, breakdown_configurations
         )
 
     return theme_distributions
 
 
-def write_theme_distributions_csv(individuals, consent_withdrawn_field, theme_configurations, survey_configurations, f):
+def write_theme_distributions_csv(individuals, consent_withdrawn_field, theme_configurations, breakdown_configurations, f):
     theme_distributions = compute_theme_distributions(individuals, consent_withdrawn_field,
-                                                      theme_configurations, survey_configurations)
+                                                      theme_configurations, breakdown_configurations)
 
     csv_data = []
     last_dataset_name = None
     for dataset_name, themes in theme_distributions.items():
-        for theme, survey_counts in themes.items():
+        for theme, breakdowns in themes.items():
             row = {
                 "Theme": theme,
                 "Dataset": dataset_name if dataset_name != last_dataset_name else ""
             }
-            row.update(survey_counts)
+            row.update(breakdowns)
             csv_data.append(row)
             last_dataset_name = dataset_name
 
     headers = ["Dataset", "Theme"]
-    headers.extend(_make_survey_counts_dict(survey_configurations).keys())
+    headers.extend(_make_breakdowns_dict(breakdown_configurations).keys())
 
     analysis_utils.write_csv(
         csv_data,
