906d50df8d5050e9e909677ffdc663cf1ff80604
Alexander Simpson <as2388@cam.ac.uk>
2021-01-25 15:23:24 +0000

    Version bump for version v0.15.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="CoreDataModules",
-    version="0.15.0",
+    version="0.15.1",
     python_requires=">=3.6.0",
     url="https://github.com/AfricasVoices/CoreDataModules",
     packages=["core_data_modules"],
