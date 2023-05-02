d3c35ed5e72af4e295a7abae84d5577c9a735fdd
Alexander Simpson <as2388@cam.ac.uk>
2023-05-02 10:44:14 +0100

    Document methods in AnalysisTestCase.py

--- tests/analysis/AnalysisTestCase.py
+++ tests/analysis/AnalysisTestCase.py
@@ -10,12 +10,31 @@ class AnalysisTestCase(unittest.TestCase):
     analysis_data = generate_synthetic_analysis_data()
 
     def setUp(self):
+        """
+        Assigns a temporary working directory to `self.test_dir`, which subclasses can use to write temporary test data
+        to.
+
+        See also: `unittest.TestCase.setUp`, which this method implements.
+        """
         self.test_dir = tempfile.mkdtemp()
 
     def tearDown(self):
+        """
+        Deletes the temporary working directory.
+
+        See also: `unittest.TestCase.tearDown`, which this method implements.
+        """
         shutil.rmtree(self.test_dir)
 
-    def assertFilesEqual(self, a, b):
-        self.assertTrue(filecmp.cmp(a, b))
+    def assertFilesEqual(self, file_path_a, file_path_b):
+        """
+        Asserts the files at the two referenced paths have contents that match exactly.
+
+        :param file_path_a: Path to the first file to compare.
+        :type file_path_a: str
+        :param file_path_b: Path to the other file to compare.
+        :type file_path_b: str
+        """
+        self.assertTrue(filecmp.cmp(file_path_a, file_path_b))
 
 
