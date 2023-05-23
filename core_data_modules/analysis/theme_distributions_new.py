23bc5cbbda9c809dc876fa67283a5807cdc97647
Alexander Simpson <as2388@cam.ac.uk>
2023-05-23 15:44:15 +0100

    Add basic reimplementation of theme_distributions

---
+++ core_data_modules/analysis/theme_distributions_new.py
@@ -0,0 +1,107 @@
+from collections import OrderedDict
+
+from core_data_modules.analysis import analysis_utils
+
+
+class ThemeAnalysis:
+    def __init__(self, scheme_id, code_id, display_text, theme_type, total_consenting_participants):
+        """
+        :type display_text: str
+        :type theme_type: str
+        :type total_consenting_participants: int
+        """
+        self.scheme_id = scheme_id
+        self.code_id = code_id
+        self.display_text = display_text
+        self.theme_type = theme_type
+        self.total_consenting_participants = total_consenting_participants
+
+    def to_dict(self):
+        return {
+            "scheme_id": self.scheme_id,
+            "code_id": self.code_id,
+            "display_text": self.display_text,
+            "theme_type": self.theme_type,
+            "total_consenting_participants": self.total_consenting_participants
+        }
+
+
+class EpisodeAnalysis:
+    def __init__(self, episode_id, total_relevant_participants, theme_distributions):
+        """
+        :type episode_id: str
+        :type total_relevant_participants: int
+        :type theme_distributions: list of ThemeAnalysis
+        """
+        self.episode_id = episode_id
+        self.total_relevant_participants = total_relevant_participants
+        self.theme_distributions = theme_distributions
+
+    def to_dict(self):
+        return {
+            "episode_id": self.episode_id,
+            "total_relevant_participants": self.total_relevant_participants,
+            "theme_distributions": [dist.to_dict() for dist in self.theme_distributions]
+        }
+
+
+class ThemeDistributions:
+    def __init__(self, episodes):
+        """
+        :type episodes: dict of str -> list of ThemeAnalysis
+        """
+        self.episodes = episodes
+
+    def to_dict(self):
+        return {
+            "episodes": [ep.to_dict() for ep in self.episodes]
+        }
+
+
+def _compute_episode_theme_distributions(participants, consent_withdrawn_field, episode_configuration,
+                                         breakdown_configurations):
+    """
+    :type episode_configuration: core_data_modules.analysis.AnalysisConfiguration
+    """
+    # Initialise theme totals for each theme in the dataset
+    theme_analyses = OrderedDict()
+    for code in episode_configuration.code_scheme.codes:
+        theme_analyses[code.code_id] = ThemeAnalysis(
+            scheme_id=episode_configuration.code_scheme.scheme_id,
+            code_id=code.code_id,
+            display_text=code.display_text,
+            theme_type=code.code_type,
+            total_consenting_participants=0
+        )
+
+    episode_total_relevant_participants = 0
+
+    # Iterate over the participants, incrementing total relevant and
+    for participant in participants:
+        if analysis_utils.relevant(participant, consent_withdrawn_field, episode_configuration):
+            episode_total_relevant_participants += 1
+
+        for code in analysis_utils.get_codes_from_td(participant, episode_configuration):
+            theme_analyses[code.code_id].total_consenting_participants += 1
+
+    return EpisodeAnalysis(
+        episode_id=episode_configuration.dataset_name,
+        total_relevant_participants=episode_total_relevant_participants,
+        theme_distributions=list(theme_analyses.values())
+    )
+
+
+def compute_theme_distributions(participants, consent_withdrawn_field, theme_configurations, breakdown_configurations):
+    participants = analysis_utils.filter_opt_ins(participants, consent_withdrawn_field)
+
+    episode_analyses = []
+    for episode_config in theme_configurations:
+        episode_analyses.append(
+            _compute_episode_theme_distributions(
+                participants, consent_withdrawn_field, episode_config, breakdown_configurations
+            )
+        )
+
+    return ThemeDistributions(
+        episodes=episode_analyses
+    )
