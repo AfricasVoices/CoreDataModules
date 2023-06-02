c5056608f62a38702a9a0bf0c6ccfcb083a14408
Alexander Simpson <as2388@cam.ac.uk>
2023-06-02 12:11:24 +0100

    Test export_theme_distributions_csv

---
+++ tests/analysis/test_theme_distributions.py
@@ -0,0 +1,16 @@
+from core_data_modules.analysis import theme_distributions
+from tests.analysis.AnalysisTestCase import AnalysisTestCase
+
+
+class TestThemeDistributions(AnalysisTestCase):
+    def test_export_theme_distributions_csv(self):
+        analysis_data = self.analysis_data
+        file_path = f"/data/theme_distributions.csv"
+
+        with open(file_path, "w") as f:
+            theme_distributions.export_theme_distributions_csv(
+                analysis_data.participants, analysis_data.CONSENT_WITHDRAWN_FIELD,
+                analysis_data.rqa_configs, analysis_data.demographic_configs, f
+            )
+
+        self.assertFilesEqual(file_path, "tests/analysis/resources/expected/expected_theme_distributions.csv")
